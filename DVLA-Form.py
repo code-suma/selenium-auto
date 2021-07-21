55a17dee910719f471b500c48fddc07967d548b6
code-suma <87770438+code-suma@users.noreply.github.com>
2021-07-21 16:12:03 +0100

    Mask PI data

--- DVLA-Form.py
+++ DVLA-Form.py
@@ -6,11 +6,11 @@ driver = webdriver.Chrome("C:\External\Suma\SQT\WebDrivers\chromedriver_win32\ch
 
 driver.get("https://forms.dft.gov.uk/order-dvla-forms/")
 driver.maximize_window()
-driver.find_element_by_name("input_1").send_keys("Rakesh")
+driver.find_element_by_name("input_1").send_keys("R****")
 #Address line 1
-driver.find_element_by_name("input_10").send_keys(" The Blenheim Centre")
+driver.find_element_by_name("input_10").send_keys(" *******entre")
 #ADDRESS LINE 2
-driver.find_element_by_name("input_43").send_keys("Prince Regent Road")
+driver.find_element_by_name("input_43").send_keys("Regency Road")
 # Town
 driver.find_element_by_name("input_4").send_keys("How")
 #cOUNTRY
@@ -18,7 +18,7 @@ driver.find_element_by_name("input_4").send_keys("How")
 
 driver.find_element_by_name("input_9").send_keys("UK")
 #POSTCODE
-driver.find_element_by_name("input_5").send_keys("TW31ND")
+driver.find_element_by_name("input_5").send_keys("****D")
 #tickbox
 time.sleep(5)
 driver.find_element_by_xpath("//input[@id='choice_23_42_1']").click()
